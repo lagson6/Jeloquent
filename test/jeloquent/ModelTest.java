7f140effc8a39096373083ed8890782752d7f0ea
Francis <lagson6@yahoo.fr>
2016-04-20 21:00:57 -0700

    Merge remote-tracking branch 'remotes/origin/myBranch'
    # Conflicts:
    #	Jeloquent.iml
    #	src/main/java/jeloquent/Model.java

---
+++ test/jeloquent/ModelTest.java
@@ -0,0 +1,46 @@
+package jeloquent;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by edwige on 4/20/16.
+ */
+public class ModelTest {
+    @Test
+    public void find() throws Exception {
+
+    }
+
+    @Test
+    public void find1() throws Exception {
+
+    }
+
+    @Test
+    public void all() throws Exception {
+
+    }
+
+    @Test
+    public void all1() throws Exception {
+
+    }
+
+    @Test
+    public void where() throws Exception {
+
+    }
+
+    @Test
+    public void create() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+}
